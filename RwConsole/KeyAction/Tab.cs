eb99c8543addad5cbde722d4f5d7d6b49d699a7e
Git0Shuai <heshuai9506@163.com>
2019-08-25 23:20:57 +0800

    Tab key action and complete engine interface.

---
+++ RwConsole/KeyAction/Tab.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RwConsole.KeyActionContext;
+
+namespace RwConsole.KeyAction
+{
+    public class Tab : KeyActionBase
+    {
+        public override void OnReadKey(ConsoleKeyInfo cki, ContextContainer ctx)
+        {
+            if (cki.Key != ConsoleKey.Tab)
+            {
+                return;
+            }
+
+            var inputBuffer = ctx.Get<InputBuffer>();
+
+            var completeResult = ctx.Get<CompleteEngine>().Complete(inputBuffer.GetInput());
+            inputBuffer.ForceSetInput(completeResult);
+            inputBuffer.ForceSetCursorPos(inputBuffer.Count);
+        }
+
+        public override void OnRegist(ContextContainer ctx)
+        {
+            if (ctx.Get<CompleteEngine>() == null)
+            {
+                ctx.Set(new CompleteEngine());
+            }
+        }
+    }
+}
---
+++ RwConsole/KeyActionContext/CompleteEngine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RwConsole.KeyActionContext
+{
+    internal class CompleteEngine : IContext
+    {
+        public CompleteEngine(List<string> candidates = null)
+        {
+
+        }
+
+        public string Complete(string prefix)
+        {
+            // TODO: complete
+            return prefix;
+            
+        }
+
+        public void AddCandidate(string candidate)
+        {
+
+        }
+    }
+
+    class DoubleArrayTrie : ICollection<string>
+    {
+        int ICollection<string>.Count => throw new NotImplementedException();
+
+        bool ICollection<string>.IsReadOnly => throw new NotImplementedException();
+
+        void ICollection<string>.Add(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<string>.Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<string>.Contains(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<string>.CopyTo(string[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<string>.Remove(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<string> IEnumerable<string>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
